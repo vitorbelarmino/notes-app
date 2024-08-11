15df1553b63e5dcca2296590ea0c16849ea12b3d
vitorbelarmino <vitor.belarmino@hotmail.com>
2024-08-11 00:35:08 -0300

    refactor: rename file

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,26 @@
 "use client";
 import Image from "next/image";
-import noteIcon from "../assets/note-icon.png";
-import searchIcon from "../assets/search-icon.png";
-import closeIcon from "../assets/close-icon.png";
+import noteIcon from "../../assets/note-icon.png";
+import searchIcon from "../../assets/search-icon.png";
+import closeIcon from "../../assets/close-icon.png";
 import { useState } from "react";
+import { notesContext } from "@/context/NoteContext";
 
 export function Header() {
   const [search, setSearch] = useState("");
+  const { searchNotes } = notesContext();
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setSearch(value);
+    searchNotes(value);
   };
 
   return (
     <header>
       <div className="flex justify-between items-center px-6 py-2 bg-white">
         <div className="flex gap-3 items-center w-[100%] mr-2">
-          <Image src={noteIcon} alt="note icon" width={36} />
+          <Image priority src={noteIcon} alt="note icon" width={36} />
           <p className="text-sm">CoreNotes</p>
           <div className="flex items-center border rounded-[3px] border-[#D9D9D9] h-7 bg-white ml-2 w-[314px] md:w-[530px] min-w-[140px] shadow-sm">
             <input
